f8cc243b3c6fb606c0501c96a7db9393af617f42
DavidBruchmann <david.bruchmann@gmail.com>
2022-10-03 16:31:25 +0700

    enrich ext_emconf.php, fix #3, add missing test cases for news and workspaces from testing framework

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,13 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '11.1.1',
+    'version' => '11.1.2',
+    'autoload' => [
+        'psr-4' => [
+            "TYPO3\\Documentation\\Screenshots\\" => "Classes/",
+            "TYPO3\\Documentation\\Screenshots\\Tests\\" => "Tests/"
+        ]
+    ],
     'constraints' => [
         'depends' => [
             'typo3' => '11.0.0-11.99.99',
@@ -17,6 +23,13 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [
         ],
         'suggests' => [
+            "friendsoftypo3/extension-builder" => "11.0.0-11.99.99",
+            "georgringer/news" => "9.0.0-10.99.99",
+            "t3docs/site-package" => "1.0.0-1.99.99",
+            "t3docs/examples" => "11.0.0-11.99.99",
+            "typo3/cms-introduction" => "4.4.1-4.99.99",
+            "typo3/cms-styleguide" => "11.0.3 - 11.99.99",
+            "typo3/cms-workspaces" => "11.0.0-11.99.99"
         ],
     ],
 ];
